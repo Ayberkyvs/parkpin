0855ff89c4bb2a4f8f879610df06a162eee26002
ayberksch <35408256+Ayberkyvs@users.noreply.github.com>
2023-05-30 02:00:57 +0300

    ss

--- js/rating.js
+++
@@ -1,7 +0,0 @@
-(function(){
-    var rating = document.querySelector('.rating');
-    var handle = document.getElementById('toggle-rating');
-    handle.onchange = function(event) {
-        rating.classList.toggle('rating', handle.checked);
-    };
-}());
